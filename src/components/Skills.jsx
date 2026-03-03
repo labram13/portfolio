8ffece4521d060a7e77f365be0a6ca76a7d51e58
labram13 <labram@uw.edu>
2026-03-02 17:47:35 -0800

    added skills to carousel

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,34 +1,89 @@
 import React from 'react'
 import js from '../assets/images/js.png'
+import node from '../assets/images/node.png'
+import react from '../assets/images/react.png'
+import docker from '../assets/images/docker.png'
+import mongo from '../assets/images/mongo.png'
+import post from '../assets/images/post.png'
+import html from '../assets/images/html.png'
+import css from '../assets/images/css.png'
 
 const SKILLS = 
-    {
-        imgUrl: js,
-        skill: "javascript"
+    [
+        {
+            imgUrl: js,
+            skill: "Javascript"
         
-    }
+        },
+        {
+            imgUrl: node,
+            skill: "Node.js"
+        
+        },
+        {
+            imgUrl: react,
+            skill: "React"
+        
+        },
+        {
+            imgUrl: docker,
+            skill: "Docker"
+        
+        },
+        {
+            imgUrl: mongo,
+            skill: "mongoDB"
+        
+        },
+        {
+            imgUrl: post,
+            skill: "PostgreSQL"
+        
+        },
+        {
+            imgUrl: html,
+            skill: "HTML"
+        
+        },
+        {
+            imgUrl: css,
+            skill: "CSS"
+        
+        }
+    ]
+
+function SkillCard(props) {
+
+    const skill = props.skill
+
+    return (
+        <li className="card">
+            <img className="tech-logo"
+            src={skill.imgUrl} alt={skill.skill + "logo"} />
+            <p className ="skill-description">{skill.skill}</p>
+        </li>
+    )
+}
+
+
 
 
 function SkillsCarousel() {
+
+    const cards = SKILLS.map((skill, index) => (
+        <SkillCard key={index} skill={skill} />
+        
+    ))
 
 
     return (
         <section>
             <h2>skills</h2>
             <div className="carousel-container">
-                <div className="carousel-track">
-                        <div className="card">1</div>
-                        <div className="card">2</div>
-                        <div className="card">3</div>
-                        <div className="card">4</div>
-
-                        {/* duplicate items */}
-
-                        <div className="card">1</div>
-                        <div className="card">2</div>
-                        <div className="card">3</div>
-                        <div className="card">4</div>
-                </div>
+                <ul className="carousel-track">
+                        {cards}
+                        {cards}
+                </ul>
             </div>
         </section>
     )
