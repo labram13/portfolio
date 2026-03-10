9f24b094fa596bc982c2d1f00ff2df472a421305
labram13 <labram@uw.edu>
2026-03-09 23:03:40 -0700

    completed contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import { useState } from 'react';
+import SendSVG from '../assets/images/send.svg?react'
 
+export default function ContactForm() {
+  const [result, setResult] = useState("");
 
-export default function DefaultForm() {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    formData.append("access_key", "a0da116e-9f7e-4f5f-8363-b997851ce995");
 
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
 
-    return (
-        <section>
-            <h2>conact</h2>
-            <form>
-                <label htmlFor="fname">First Name</label>
-                {/* <input type="text" id="fname" name="fname"/> */}
-            </form>
-        </section>
-    )
+    const data = await response.json();
+    setResult(data.success ? "Success!" : "Error");
+  };
+
+  return (
+    <section>
+        <h2>contact</h2>
+
+        <form onSubmit={onSubmit}>
+        <input type="text" name="name" placeholder='Name' required/>
+        <input type="email" name="email" placeholder='Email' required/>
+        <textarea name="message" placeholder='Message' required></textarea>
+        <button type="submit"><SendSVG /><p>Send</p></button>
+        <p>{result}</p>
+        </form>
+    </section>
+  );
 }
