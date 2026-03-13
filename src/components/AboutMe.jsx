b744c8dee653ff34c5ec7e2d8fe256bffda9d308
labram13 <labram@uw.edu>
2026-03-13 11:37:56 -0700

    updated about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,7 +6,7 @@ export function AboutMe() {
     return (
         <section id="about">
             <h2>About Me</h2>
-            <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat.</p>
+            <p className="description">Hello! My name is Michaelangelo Labrador. I'm a recent graduate with a degree in <span >Informatics</span> focused in Software Developtment. Within my department, I participated in group projects that mirrored real <span>industry practices</span>. Experience includes building applications, both web and mobile apps, using methodologies such as <span>Agile</span>. Along with learning <span>research design processes</span>, I've learned to gather important market research and shareholder information in order to fulfill the needs of a targeted user audience. Today, I'm building on those skills learning new tech stacks and honing my coding abilities with personal projects inspired by my <span>passionate hobbies</span>.  </p>
         </section>
         
     )
