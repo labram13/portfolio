e57300f403711dcf6bf1223a728e7eef6257731c
labram13 <labram@uw.edu>
2026-03-13 15:30:35 -0700

    added emt and dose projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,16 +2,33 @@ import React from 'react';
 import LiveSVG from '../assets/images/live.svg?react'
 import CodeSVG from '../assets/images/code.svg?react'
 import quest from '../assets/images/quest.png'
+import emt from '../assets/images/emt.png'
+import dose from '../assets/images/dose.png'
 
 const PROJECTS = [
     {
-        title: "Quest Diaries", 
+        title: "Quest Diaries 2026", 
         techStack: ['React', 'MongoDB', 'Express', 'nodeJS', 'IGDB', 'GoogleOAuth', 'Javascript'],
         description: "A web-application for gamers with a focus on organizing their games, logging their experiences, as well as interacting with a community of other like-minded individuals. Community interaction among users that helps recognition of both mainstream and indie games alike. Users can choose to journal individually, create shared journals, and interact with other users in gaming communities.",
         live: "https://quest-diaries.onrender.com",
         code: "https://github.com/labram13/quest-diaries",
         img: quest
 
+    },
+    {
+        title: "Employee Management Tool 2025", 
+        techStack: ['Express', 'MongoDB', 'Express', 'nodeJS', 'Azure App Service'],
+        description: "API tool that is designed for business owners who want to keep track of employee information and financial data to calculate accurate salaries. If they input hourly wages, tips, and bonuses, the application will automatically calculate the total based on hours worked. This ensures that all wages are accurate, allowing owners to easily check how many hours employees worked and their hourly wages. It would let owners filter an individual's payroll for that month, or over a certain time period. Allow business owners to input, process and output financial information for employees for bookkeeping.",
+        code: "https://github.com/info441-au24/final-project-wena04",
+        img: emt
+    },
+    {
+        title: "Dose 2023",
+        techStack: ['React', 'Javascript', 'Firebase'],
+        description: "Client-side focused application with a focus in medication. App is designed to help users organize their medication by days of the week with required doses. Separate API is used to fetch medication information based on user query. Side-effects are included with each medication added to users pill box within the app in order to be aware proper medication intake.",
+        img: dose,
+        live: "https://project-a5-f5b82.web.app/",
+        code: "https://github.com/info340-su23/project-GConscious"
     }
 ]
 
@@ -38,7 +55,7 @@ function ProjectCard(props) {
 
     return (
         <article className="project-card">
-            <img src={project.img} />
+            <img src={project.img} alt={`${project.title} screenshot`}/>
             <h3 className="project-title">{project.title}</h3>
             <div className="project-tech-stack">
                 {project.techStack.map((tech) => (<p key={tech}>{tech}</p>))}
@@ -47,10 +64,10 @@ function ProjectCard(props) {
          
             <div className="project-links">
                 <a href="https://quest-diaries.onrender.com/">
-                    <button className="live-button">
+                    {project.live && (<button className="live-button">
                         <LiveSVG />
                         <p>Live</p>
-                    </button>
+                    </button>)}
                 </a>
                 <a href="https://github.com/labram13/quest-diaries">
                     <button className="code-button">
@@ -58,7 +75,6 @@ function ProjectCard(props) {
                         <p>Code</p>
                     </button>
                 </a>
-
             </div>
         </article>
     )
