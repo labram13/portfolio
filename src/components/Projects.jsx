487a8c030a79b8afc6141aff929f1657538fffbf
labram13 <labram@uw.edu>
2026-03-13 14:56:04 -0700

    edited quest diaries description

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,7 @@ const PROJECTS = [
     {
         title: "Quest Diaries", 
         techStack: ['React', 'MongoDB', 'Express', 'NodeJS', 'IGDB', 'GoogleOAuth'],
-        description: "Our project's goal is to develop a web-application for gamers with a focus on organizing their games, logging their experiences for each game, as well as interacting with a community of other like-minded individuals. Community interaction among users in the website will also hopefully encourage more recognition of both mainstream and indie games alike. Users can choose to journal individually, create shared journals, and interact with other users in gaming communities.",
+        description: "A web-application for gamers with a focus on organizing their games, logging their experiences, as well as interacting with a community of other like-minded individuals. Community interaction among users that helps recognition of both mainstream and indie games alike. Users can choose to journal individually, create shared journals, and interact with other users in gaming communities.",
         live: "https://quest-diaries.onrender.com",
         code: "https://github.com/labram13/quest-diaries",
         img: quest
@@ -41,11 +41,6 @@ function ProjectCard(props) {
             <img src={project.img} />
             <h3 className="project-title">{project.title}</h3>
             <div className="project-tech-stack">
-                {/* <p>Label</p>
-                <p>Label</p>
-                <p>Label</p>
-                <p>Label</p>
-                <p>Label</p> */}
                 {project.techStack.map((tech) => (<p key={tech}>{tech}</p>))}
             </div>
             <p className="project-description">{project.description}</p>
