5a90b943ca44a7345f42581c7b7dc98a99ffdfa2
labram13 <labram@uw.edu>
2026-03-16 16:17:57 -0700

    added tech stack

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,7 +10,7 @@ import chatterbox from '../assets/images/chatterbox.png'
 const PROJECTS = [
     {
         title: "chatterbox (In Progress)", 
-        techStack: ['React', 'PostgreSQL', 'Express', 'Node.js', 'AWS Storage', 'JWT', 'JavaScript', 'Redis', 'Socket.io'],
+        techStack: ['React', 'PostgreSQL', 'Express', 'Node.js', 'AWS Storage', 'JWT', 'JavaScript', 'Redis', 'Socket.io', 'Docker'],
         description: "A full-stack chat chat application with a goal of sending messages to individuals or a group of people within a created chatroom. The goal of this project is to apply knowledge of websockets for instant communication and notifications. Caching will also be implemented for faster message load times. Cloud file storage will be included so users will be able to send different types of media mostly aimed at images. ",
         code: "https://github.com/labram13/ChatterBox",
         img: chatterbox
